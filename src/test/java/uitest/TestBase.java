7af82f93147721a49bc9fff8e003341b462250db
John Smoker <testgits@yandex.ru>
2021-03-23 05:43:52 +0500

    Added Listener

--- src/test/java/uitest/TestBase.java
+++ src/test/java/uitest/TestBase.java
@@ -1,6 +1,8 @@
 package uitest;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -9,5 +11,6 @@ public class TestBase {
         static void setup() {
             Configuration.startMaximized = true;
             Configuration.baseUrl = "https://www.saucedemo.com";
+            SelenideLogger.addListener("allure", new AllureSelenide().screenshots(true));
         }
 }
